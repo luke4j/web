b1d14608c8ca5058d23a0f91d3342d15a6cfff61
llg6yy@163.com <llg6yy@163.com>
2019-06-20 16:37:53 +0800

    工具更新一个方法

--- tool/src/main/java/com/luke/web/tool/LK.java
+++ tool/src/main/java/com/luke/web/tool/LK.java
@@ -15,6 +15,7 @@ import org.apache.commons.jexl2.MapContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 import java.security.MessageDigest;
 import java.text.DecimalFormat;
@@ -498,5 +499,36 @@ public class LK {
         return e.evaluate(jc);
     }
 
+
+
+    // 浏览器类型
+    private static final String[] mobileAgents = { "iphone", "android", "phone", "mobile", "wap", "netfront", "java",
+            "opera mobi", "opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry",
+            "dopod", "nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma",
+            "docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos",
+            "techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem", "wellcom",
+            "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos", "pantech", "gionee",
+            "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320", "240x320", "176x220", "w3c ",
+            "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac", "blaz", "brew", "cell", "cldc",
+            "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs", "kddi", "keji", "leno", "lg-c",
+            "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi", "mot-", "moto", "mwbp", "nec-",
+            "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port", "prox", "qwap", "sage", "sams",
+            "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem", "smal", "smar", "sony", "sph-",
+            "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v", "voda", "wap-", "wapa", "wapi",
+            "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-", "Googlebot-Mobile" };
+
+
+    public static boolean isMobile (HttpServletRequest request) {
+        boolean isMobile = false;
+        if (request.getHeader("User-Agent") != null) {
+            for (String mobileAgent : mobileAgents) {
+                if (request.getHeader("User-Agent").toLowerCase().indexOf(mobileAgent) > 0) {
+                    isMobile = true;
+                    break;
+                }
+            }
+        }
+        return isMobile;
+    }
 
 }
